386fd2e7f0bd032b11eb4b3a0564c3d5d43d9231
User <user@Kates-MacBook-Pro.local>
2016-06-05 11:21:23 -0500

    grunt file add

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,19 +2,19 @@ module.exports = function (grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            controllers: {
-                src: ['client/assets/scripts/admin/controllers/*.js', 'client/assets/scripts/teacher/controllers/*.js'],
-                dest: 'server/public/assets/scripts/controllers.min.js'
-            },
-            factories: {
-                src: ['client/assets/scripts/admin/factories/*.js', 'client/assets/scripts/teacher/factories/*.js'],
-                dest: 'server/public/assets/scripts/factories.min.js'
-            }
-        },
+        // uglify: {
+        //     options: {
+        //         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        //     },
+        //     controllers: {
+        //         src: ['client/scripts/controllers/*.js'],
+        //         dest: 'server/public/scripts/controllers.min.js'
+        //     },
+        //     factories: {
+        //         src: ['client/scripts/factories/*.js'],
+        //         dest: 'server/public/scripts/factories.min.js'
+        //     }
+        // },
         copy: {
             vendor: {
                 expand: true,
@@ -24,56 +24,25 @@ module.exports = function (grunt) {
                 cwd: "node_modules/",
                 src: [
                     "angular/angular.min.js",
-                    "angular/angular.min.js.map",
-                    "angular-animate/angular-animate.min.js",
-                    "angular-animate/angular-animate.min.js.map",
-                    "angular-aria/angular-aria.min.js",
-                    "angular-aria/angular-aria.min.js.map",
-                    "angular-material/angular-material.min.js",
-                    "angular-material/angular-material.min.js.map",
-                    "angular-material/angular-material.min.css",
-                    "angular-material/angular-material.layouts.min.css",
-                    "angular-messages/angular-messages.min.js",
-                    "angular-messages/angular-messages.min.js.map",
-                    "angular-route/angular-route.min.js",
-                    "angular-route/angular-route.min.js.map",
-                    "angular-ui-grid/ui-grid.min.css",
-                    "angular-ui-grid/ui-grid.min.js",
-                    "ng-file-upload/dist/ng-file-upload.min.js",
-                    "angular-touch/angular-touch.min.js",
-                    "angular-touch/angular-touch.min.js.map"
+                    "bootstrap/dist/css/bootstrap.min.css"
                 ],
                 "dest": "server/public/vendors/"
-            },
-
-            font: {
-                expand: true,
-                cwd: "node_modules/angular-ui-grid/",
-                src: ["ui-grid.ttf", "ui-grid.woff"],
-                "dest": "server/public/vendors/angular-ui-grid/"
-            },
-
-            pdfmake: {
-                expand: true,
-                cwd: "client/assets/pdfmake",
-                src: "**",
-                "dest": "server/public/vendors/pdfmake"
             },
 
             css: {
                 // STYLES
                 expand: true,
-                cwd: "client/assets/styles/",
-                src: "style.css",
-                "dest": "server/public/assets/styles"
+                cwd: "client/styles/",
+                src: "stylesheet.css",
+                "dest": "server/public/styles"
             },
             // IMAGES
-            images: {
-                expand: true,
-                cwd: "client/images/",
-                src: "**",
-                "dest": "server/public/images"
-            },
+            // images: {
+            //     expand: true,
+            //     cwd: "client/images/",
+            //     src: "**",
+            //     "dest": "server/public/images"
+            // },
 
             html: {
                 // VIEWS
@@ -86,9 +55,9 @@ module.exports = function (grunt) {
             scripts: {
                 // SCRIPTS
                 expand: true,
-                cwd: "client/assets/scripts/",
-                src: ["*.js", "admin/admin.js", "teacher/teacher.js"],
-                "dest": "server/public/assets/scripts"
+                cwd: "client/scripts/",
+                src: ["*.js"],
+                "dest": "server/public/scripts"
             }
         },
 
@@ -98,17 +67,18 @@ module.exports = function (grunt) {
             },
             scripts: {
                 files: ['client/**/*.html', 'client/**/*.js', 'client/**/*.css'],
-                tasks: ['copy', 'uglify']
+                // tasks: ['copy', 'uglify']
+                tasks: ['copy']
             }
         }
     });
 
 
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    // grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task(s).
-    grunt.registerTask('default', ['copy', 'uglify', 'watch']);
+    grunt.registerTask('default', ['copy', 'watch']);
 
 };
