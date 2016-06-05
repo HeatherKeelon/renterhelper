dd88f3d9e79b15b130506091c98aa7c1a79b9848
Moni Francesca <mo.francesca@gmail.com>
2016-06-05 11:51:36 -0500

    merging

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
-var index = require('./routes/index');
+var index = require('./server/routes/index');
 
 
 //route modules
